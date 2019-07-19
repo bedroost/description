1d9ca4d332b94dd9eb1d2a9207a42c95847254db
Yunyun <celeph4is@gmail.com>
2019-07-19 14:14:49 -0700

    add new mongo uri string for connecting to mongo cluster

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const Description = require('./Description.js');
 const generateFakeDescriptions = require('./seed.js');
 
-mongoose.connect('mongodb://localhost:27017/descriptions');
+mongoose.connect('mongodb+srv://admin:description@listingdescriptions-omqwn.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
