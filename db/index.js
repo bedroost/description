6707b832dcfe668b5a743486591d113ed4beead8
Yunyun <celeph4is@gmail.com>
2019-07-19 21:03:23 -0700

    clean up code

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable func-names */
 /* eslint-disable arrow-body-style */
 const mongoose = require('mongoose');
-const Description = require('./Description.js');
-const generateFakeDescriptions = require('./seed.js');
+// const Description = require('./Description.js');
+// const generateFakeDescriptions = require('./seed.js');
 
 mongoose.connect('mongodb+srv://admin:description@listingdescriptions-omqwn.mongodb.net/descriptions?retryWrites=true&w=majority');
 
@@ -10,15 +10,15 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', async () => {
   console.log('Mongoose connected to database');
-  db.dropCollection('descriptions');
-  console.log('deleted');
-  const docs = generateFakeDescriptions();
-  try {
-    await Description.create(docs);
-    console.log('inserted');
-  } catch (err) {
-    console.error(err);
-  }
 });
 // db.once('open', async () => {
 //   console.log('mongoose connected to database');
+// db.dropCollection('descriptions');
+// console.log('deleted');
+// const docs = generateFakeDescriptions();
+// try {
+//   await Description.create(docs);
+//   console.log('inserted');
+// } catch (err) {
+//   console.error(err);
+// }
